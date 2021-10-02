f72dc916ebed3ae8d6e9807f4b5bd03bb8625b4c
liuchengkai <710403197@qq.com>
2021-10-02 20:07:48 +0800

    add npm scripts, dev and prod config
    "npm run build" with production config and "npm run serve" with development config

---
+++ webpack.config.prod.js
@@ -0,0 +1,43 @@
+const path = require('path')
+module.exports = {
+    mode: 'production',
+    entry: path.join(__dirname, 'src', 'web-pet.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'web-pet.js',
+        library: {
+            name: 'WebPet',
+            type: "var"
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'url-loader',
+                options: {
+                    name: './resources/img/[name].[ext]',
+                    limit: 8192,
+                    publicPath: './resources/img/'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader',  
+                {
+                    loader: 'css-loader',
+                    options: {
+                        url: false
+                    }
+                }],
+            }
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './dist'),
+          },
+        port: 9000,
+        hot: true,
+    },
+}
